fb0832b488cb307548097045a193847d01b77714
MirzaAyon <ayonmirza98@gmail.com>
2022-06-09 21:00:19 +0600

    logarithmsN added

---
+++ logarithmsN.js
@@ -0,0 +1,10 @@
+function logarithmsN(n) {
+    while (n > 1) {
+        console.log(n);
+        n /= 2
+    }
+    console.log('Done');
+}
+logarithmsN(8);
+logarithmsN(25);
+//log er maan ke 2 dara devide korte hbe and joto khon prjnto 1 na ashe toto khon prjnto divide korte thakbo
